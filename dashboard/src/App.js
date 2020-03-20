ba920b9cb3778078508842c170046555ffa282b7
Nathalia <indigodreamer11@hotmail.com>
2020-03-20 14:23:26 -0500

    Layout

--- dashboard/src/App.js
+++ dashboard/src/App.js
@@ -1,26 +1,41 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {Layout, Avatar} from 'antd';
+import Title from 'antd/lib/typography/Title';
+import {DesktopOutlined, PieChartOutlined,FileOutlined,TeamOutlined,UserOutlined} from '@ant-design/icons'
+
+const { Header, Content, Footer, Sider } = Layout;
 
 function App() {
-  return (
+
+
+  
+  return(
+
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Layout>
+        <Header style={{padding:10}}>
+        <Avatar style={{float:'right'}} icon={<UserOutlined />} />
+          <Title style={{color:'white'}} level={3}>CATIXA</Title>
+        </Header>
+        <Layout>
+          <Sider style={{background:'red'}}>Sider</Sider>
+          <Layout>
+            <Content>
+              Content
+            </Content>
+            <Footer>Footer</Footer>
+          </Layout>
+        </Layout>
+      </Layout>
     </div>
   );
 }
 
 export default App;
+
+
+
+
+
