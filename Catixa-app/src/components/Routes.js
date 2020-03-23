b51c7e7d6374da5e27e1bfc71be3cc7f957e6a70
Oscaralsa <oscaralsa@unisabana.edu.co>
2020-03-22 21:41:14 -0500

    Catixa-App

---
+++ Catixa-app/src/components/Routes.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import {Router, Stack, Scene} from 'react-native-router-flux';
+
+import Login from '../pages/Login';
+import Rate from '../pages/Rate';
+
+
+export default class Routes extends Component<{}> {
+	render() {
+		return(
+			<Router>
+			    <Scene>
+							<Scene key="root" hideNavBar={true} initial={!this.props.isLoggedIn}>
+								<Scene key="login" component={Login} initial={true} />
+								
+							</Scene>
+							<Scene key="app" hideNavBar={true} initial={this.props.isLoggedIn}>
+								<Scene key="Rate" component={Rate} />
+							</Scene>
+							
+					</Scene>
+			 </Router>
+			)
+	}
+}
---
+++ Catixa-app/src/config/firebase.js
@@ -0,0 +1,17 @@
+import  firebase from "firebase";
+import 'firebase/database';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyD575l_oaGodJjih78MAj8GGFWKagnYug8",
+    authDomain: "catixa-6cf9b.firebaseapp.com",
+    databaseURL: "https://catixa-6cf9b.firebaseio.com",
+    projectId: "catixa-6cf9b",
+    storageBucket: "catixa-6cf9b.appspot.com",
+    messagingSenderId: "1014629775674",
+    appId: "1:1014629775674:web:be7261f9d2e8dbfa4ab803",
+    measurementId: "G-JQL1ZS5371"
+  };
+
+  firebase.initializeApp(firebaseConfig);
+
+export default firebaseConfig;
